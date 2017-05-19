4fc2a93a3668b9b32ffc9c5eee36a32d5ef08395
Nate Kem <natek@Nates-MacBook>
2017-05-18 23:55:19 -0700

    [ Models ] Added documentation/comment for BaseModel.js.

--- src/models/BaseModel.js
+++ src/models/BaseModel.js
@@ -2,6 +2,7 @@
 * BaseModel.js
 * File: models/BaseModel.js
 * BaseModel for any data. It is just the representation of the data and nothing else.
+* The goal is to mapping attributes/data from DAO object. There is no direct interaction with DB.
 * @author Nate K.
 */
 'use strict';
@@ -11,12 +12,18 @@ class BaseModel {
     this.id = ''; // ID for look up from DB
     this.created_at = ''; // Unix timestamp
     this.updated_at = ''; // Unix timestamp
+    this.cas = ''; // CAS value
   }
 
+  /** Serialize this object to JSON string. */
   ToJSON() {
     return JSON.stringify(this);
   }
 
+  /**
+  * Deserialize JSON string to this object.
+  * @param jsonStr      String in JSON format
+  */
   FromJSON(jsonStr) {
     var obj = JSON.parse(jsonStr);
 
