d36d52ef5f56b385de95d1584c4cb83a05619a53
Nate Kem <me@nateserk.com>
2017-06-07 19:52:58 -0700

    [ Scripts | Google Data Store ] Added comment to 'gdatastore_indexing.sh' indexing script.

--- scripts/gdatastore_indexing.sh
+++ scripts/gdatastore_indexing.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+# Google DataStore Indexing Script
+# This will fetch the index configuration from `index.yaml` and push it to Google Cloud.
+# Author: Nate K. (nkemavaha@gmail.com)
+#
 # See https://cloud.google.com/datastore/docs/tools/#the_development_workflow_using_gcloud
-# Make sure to run `gcloud init` to point to the correct project.
+#
+# NOTE:
+# - Make sure to run `gcloud init` to have configuration pointing to the correct project.
 
 # TODO: Make it a command line (create vs delete)
 
